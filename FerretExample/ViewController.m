431d6a8ce517f14746498c3321ebcf3f8830938b
Ryosuke Ito <rito.0305@gmail.com>
2020-06-16 23:21:16 +0900

    Initial commit

---
+++ FerretExample/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FerretExample
+//
+//  Created by Ryosuke Ito on 6/16/20.
+//  Copyright © 2020 Ryosuke Ito. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

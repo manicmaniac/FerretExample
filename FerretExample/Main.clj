5f437095385efa41bff9f65f097d844e0cc74b38
Ryosuke Ito <rito.0305@gmail.com>
2020-06-17 00:57:08 +0900

    Invoke main function from lisp code

--- FerretExample/Main.clj
+++ FerretExample/Main.clj
@@ -1,4 +1,10 @@
 (native-declare "#import <UIKit/UIKit.h>")
+(native-declare "#import \"AppDelegate.h\"")
 (native-declare "#undef nil")
 
-(cxx "UIApplicationMain(0, NULL, NULL, @\"AppDelegate\")")
+(defn uiapplication-main [args, delegate-class-name]
+    "std_vector _args = sequence::to<std_vector>(args);
+     int status = ::UIApplicationMain((int)_args.size(), reinterpret_cast<char **>(_args.data()), nullptr, @(string::c_str(delegate_class_name)));
+     exit(status);")
+
+(uiapplication-main *command-line-args* "AppDelegate")

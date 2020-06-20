b9c75f68c431c1092fddb374238cf32d789e895c
Ryosuke Ito <rito.0305@gmail.com>
2020-06-20 16:34:10 +0900

    Add custom view controller

--- FerretExample/core.clj
+++ FerretExample/core.clj
@@ -1,6 +1,20 @@
 (require '[objc :as objc])
 (require '[uikit :as ui])
 
+(let [UIViewController (objc/objc-get-class "UIViewController")
+      ViewController (objc/objc-allocate-class-pair UIViewController "ViewController", 0)]
+  (objc/objc-register-class-pair ViewController)
+  (objc/class-add-method ViewController
+                         "viewDidLoad"
+                         (objc/imp-implementation-with-ferret-lambda
+                           0
+                           (fn [self]
+                             (let [view (objc/objc-msg-send self "view")
+                                   UIColor (objc/get-class "UIColor")
+                                   green (objc/objc-msg-send UIColor "greenColor")]
+                               (objc/objc-msg-send view "setBackgroundColor:" green))))
+                         "@:"))
+
 (let [UIResponder (objc/objc-get-class "UIResponder")
       AppDelegate (objc/objc-allocate-class-pair UIResponder "AppDelegate", 0)]
   (objc/objc-register-class-pair AppDelegate)
@@ -15,10 +29,8 @@
                                      viewController (objc/objc-msg-send (objc/objc-msg-send UIViewController "alloc") "init")
                                      view (objc/objc-msg-send viewController "view")]
                                  (objc/objc-msg-send window "setRootViewController:" viewController)
-                                 (let [UIColor (objc/objc-get-class "UIColor")
-                                       green (objc/objc-msg-send UIColor "greenColor")]
-                                   (objc/objc-msg-send view "setBackgroundColor:" green)))
                                (objc/objc-msg-send window "makeKeyAndVisible"))
                              true))
                          "@:@@"))
+
 (ui/application-main *command-line-args* "AppDelegate")

d57d83007deff46b49d73ea3e765259e35d93252
Ryosuke Ito <rito.0305@gmail.com>
2020-06-21 18:43:57 +0900

    Refactor in Clojure manner

--- FerretExample/core.clj
+++ FerretExample/core.clj
@@ -1,33 +1,34 @@
 (require 'objc 'uikit)
 
+(def *flexible-width-and-height*
+  (objc/int (bit-or (bit-shift-left 1 1)
+                    (bit-shift-left 1 4))))
+
+(def *scale-aspect-fit* (objc/int 1))
+
 (doto (objc/defclass "ViewController" "UIViewController")
   (objc/defmethod "viewDidLoad" "@:"
     (fn [self]
-      (let [view (objc/send self "view")]
-        (let [color (-> (objc/class-of "UIColor")
-                        (objc/send "systemGrayColor"))]
-          (objc/send view "setBackgroundColor:" color))
-        (let [image (-> (objc/class-of "UIImage")
-                        (objc/send "imageNamed:" (objc/str "ferret.png")))
-              imageView (doto (-> (objc/class-of "UIImageView")
-                                  (objc/send "alloc")
-                                  (objc/send "initWithImage:" image))
-                          (objc/send "setAutoresizingMask:" (objc/int (bit-or (bit-shift-left 1 1)
-                                                                                              (bit-shift-left 1 4))))
-                          (objc/send "setContentMode:" (objc/int 1)))]
-          (objc/send view "addSubview:" imageView))))))
+      (doto (objc/send self "view")
+        (objc/send "setBackgroundColor:" (-> (objc/class-of "UIColor")
+                                             (objc/send "systemGrayColor")))
+        (objc/send "addSubview:" (doto (-> (objc/class-of "UIImageView")
+                                           (objc/send "alloc")
+                                           (objc/send "initWithImage:" (-> (objc/class-of "UIImage")
+                                                                           (objc/send "imageNamed:" (objc/str "ferret.png")))))
+                                   (objc/send "setAutoresizingMask:" *flexible-width-and-height*)
+                                   (objc/send "setContentMode:" *scale-aspect-fit*)))))))
 
 (doto (objc/defclass "AppDelegate" "UIResponder")
   (objc/defmethod "application:didFinishLaunchingWithOptions:" "@:@@"
     (fn [self application options]
-      (let [window (-> (objc/class-of "UIWindow")
-                       (objc/send "alloc")
-                       (objc/send "init"))]
-        (let [viewController (-> (objc/class-of "ViewController")
-                                 (objc/send "alloc")
-                                 (objc/send "init"))]
-          (objc/send window "setRootViewController:" viewController))
-        (objc/send window "makeKeyAndVisible"))
+      (doto (-> (objc/class-of "UIWindow")
+                (objc/send "alloc")
+                (objc/send "init"))
+        (objc/send "setRootViewController:" (-> (objc/class-of "ViewController")
+                                                (objc/send "alloc")
+                                                (objc/send "init")))
+        (objc/send "makeKeyAndVisible"))
       objc/*yes*)))
 
 (uikit/uiapplication-main *command-line-args* "AppDelegate")
